2dd7976fb4b87f2c5d2c9983215bdd3dc1a88419
CommitTODev <abhishekshivale45@gmail.com>
2024-01-06 17:41:38 +0530

    done project

--- backend/register.js
+++ backend/register.js
@@ -1,46 +1,44 @@
 const express = require('express');
-const router = express.Router();
-router.use(express.json());
-const registerDb = require('./db');
+const router = express.Router(); 
+router.use(express.json()); 
+const registerDb = require('./db')
 const schemaZod = require('./zod');
+const { default: mongoose } = require('mongoose');
 
-router.post('/login', async (req, res) => {
-  const reqForlogin = req.body;
-  console.log(reqForlogin);
-  const verifyLogin = schemaZod.loginZod.parse(reqForlogin);
-  if (verifyLogin.error) {
-    res.status(411).json({
-      msg: 'You sent a wrong input',
-      error,
-    });
-  }
-  try {
-    const user = await registerDb.register
-      .findOne({ email: reqForlogin.email })
-      .exec();
-
-    if (user) {
-      const isMatch = reqForlogin.password === user.password;
-      if (isMatch) {
-        console.log('Login successful');
-        res.json({
-          msg: 'Login successful',
-        });
-      } else {
-        console.log('Incorrect Credential');
-        res.json({
-          msg: 'Incorrect Credential',
-        });
-      }
-    } else {
-      console.log('User not found');
-      res.json({
-        msg: 'User not found',
-      });
+router.post('/register',async (req,res)=>{
+    // const {email,username,password} = req.body;
+    const allReq = req.body;
+    const verifyReq = schemaZod.registerZod.safeParse(allReq)
+    if(verifyReq.error){
+        res.status(411).json({
+            msg: "You sent a wrong input"
+        })
     }
-  } catch (error) {
-    console.error(error);
-  }
-});
+    const email = verifyReq.email
+   try{ 
+
+    const already = await registerDb.register.findOne({email:email})
+    if(already){
+        res.json({
+            msg: "User alredy exists"
+        })
+
+    }
+    }catch(err){
+        console.error(err)
+    }
+
+    try{
+        await registerDb.register.create({
+            userName: verifyReq.username,
+            email: email,
+            password: verifyReq.password
+        },res.json({
+           meg: "your account has been created"
+        }))
+    }catch(err){
+        console.error(err);
+    }
+})
 
 module.exports = router;
--- frontend/src/component/register.jsx
+++ frontend/src/component/register.jsx
@@ -1,76 +1,76 @@
-// import React, { useState } from 'react'
-// import './register.css'
-// import { application } from 'express';
-// function register() {
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-//   const [username, setUsername] = useState('')
-//   const [update, setUpdate] = useState('');
-//   const tryRegister = async () =>{
-//     if(password.length>= 8){
-//       const data = await fetch('http://localhost:3001/register',{
-//         method:'POST',
-//         body: JSON.stringify({
-//           username: username,
-//           email: email,
-//           password: password
-//         }),
-//         headers:{'content-type': 'application/json'}
-//       })
-//        console.log('---------->data', data);
-//     const jsonData = await data.json();
+import React, { useState } from 'react'
+import './register.css'
+function register() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('')
+  const [update, setUpdate] = useState('');
+  const tryRegister = async () =>{
+    if(password.length>= 8){
+      const data = await fetch('http://localhost:3001/register',{
+        method:'POST',
+        body: JSON.stringify({
+          username: username,
+          email: email,
+          password: password
+        }),
+        headers:{'content-type': 'application/json'}
+      })
+       console.log('---------->data', data);
+    const jsonData = await data.json();
 
-//     console.log('---------->jsonData', jsonData);
-//     setUpdate(jsonData.msg);
-//     }else{
-//       setUpdate('A Password must contain 8 characters')
-//     }
-//   }
-//   return (
-// <div className='register-container'>
-//     <div className='register-form'>
-//      <h1>Register</h1>
-//      <div className='input-group'>
-//       <label htmlFor='username'>Your UserName</label>
-//       <input 
-//       type='text'
-//       id='username'
-//       required
-//       placeholder='Enter Your Username'
-//       className='input-field'
-//       onChange={(e)=>{
-//         setUsername(e.target.value)
-//       }}
-//       value={username}
-//       />
+    console.log('---------->jsonData', jsonData);
+    setUpdate(jsonData.msg);
+    }else{
+      setUpdate('A Password must contain 8 characters')
+    }
+  }
+  return (
+<div className='register-container'>
+    <div className='register-form'>
+     <h1>Register</h1>
+     <div className='input-group'>
+      <label htmlFor='username'>Your UserName</label>
+      <input 
+      type='text'
+      id='username'
+      required
+      placeholder='Enter Your Username'
+      className='input-field'
+      onChange={(e)=>{
+        setUsername(e.target.value)
+      }}
+      value={username}
+      />
 
-//      </div>
-//       <div className='input-group'> 
-//       <label htmlFor='email'>Email</label>
-//       <input
-//       type='email'
-//       id='email'
-//       required
-//       placeholder='Enter your Email'
-//       onChange={(e)=>{setEmail(e.target.value)}} value={email}/>
-//     </div>
-//     <div className='input-group'>
-//       <label htmlFor='password'>PassWord</label>
-//       <input type='password'
-//       id='password' required 
-//       placeholder='Enter Your Password'className='input-field'
-//       onChange={(e)=>{setPassword(e.target.value)}}value={password}/>
-//     </div>
-//     <button type='submit' className='register-button' onClick={tryRegister}>Register</button>
-//     <p className='login-link'>
-//       Already have an accounnt?<a href='/login'>Login</a>
-//     </p>
-//     {update && <p style={{color:'red'}}>{update}</p>}
-//   </div>
-// </div>
+     </div>
+      <div className='input-group'> 
+      <label htmlFor='email'>Email</label>
+      <input
+      type='email'
+      id='email'
+      className='input-field'
+      required
+      placeholder='Enter your Email'
+      onChange={(e)=>{setEmail(e.target.value)}} value={email}/>
+    </div>
+    <div className='input-group'>
+      <label htmlFor='password'>PassWord</label>
+      <input type='password'
+      id='password' required 
+      placeholder='Enter Your Password'className='input-field'
+      onChange={(e)=>{setPassword(e.target.value)}}value={password}/>
+    </div>
+    <button type='submit' className='register-button' onClick={tryRegister}>Register</button>
+    <p className='login-link'>
+      Already have an accounnt?<a href='/login'>Login</a>
+    </p>
+    {update && <p style={{color:'red'}}>{update}</p>}
+  </div>
+</div>
 
 
-//   )
-// }
+  )
+}
 
-// export default register
+export default register
